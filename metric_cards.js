d52e0e134976e76742e594b785d05522438a15dd
fialajonas <mail>
2026-06-08 22:28:32 +0200

    .

---
+++ metric_cards.js
@@ -0,0 +1,9 @@
+function metric_card() {
+    `<div class="mcard">
+        <div class="mcard-label">Total Return</div>
+        <div id="return" class="mcard-value pos">+187.4%</div>
+        <div class="mcard-sub">
+            vs SPY <span class="pos">+112.3%</span>
+        </div>
+    </div>`
+}
