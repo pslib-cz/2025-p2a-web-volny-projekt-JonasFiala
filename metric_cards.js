092eeb63fe955f1982d15b7d46ae80b3d71fb149
unknown <jonas.fiala.024@pslib.cz>
2026-06-09 08:41:22 +0200

    .

--- metric_cards.js
+++ metric_cards.js
@@ -1,9 +1,2 @@
-function metric_card() {
-    `<div class="mcard">
-        <div class="mcard-label">Total Return</div>
-        <div id="return" class="mcard-value pos">+187.4%</div>
-        <div class="mcard-sub">
-            vs SPY <span class="pos">+112.3%</span>
-        </div>
-    </div>`
+function metric_card(name) {
 }
